2f042ed7f33aa607f2217205f751b8b4a0a8f1c9
Kimbbakar <chowdhuryosman04@gmail.com>
2018-11-21 18:39:51 +0600

    add home view

--- scrap/views.py
+++ scrap/views.py
@@ -1,3 +1,9 @@
+import os,sys
 from django.shortcuts import render
+SCRIPT_DIR =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SCRIPT_DIR = os.path.join(SCRIPT_DIR, 'script'); 
+sys.path.append(SCRIPT_DIR)
 
-# Create your views here.
+
+def home(request):
+	return render(request,'home.html')
