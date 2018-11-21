aa3e0536db8a48b1b7eb9057031e996df56b9aae
Kimbbakar <chowdhuryosman04@gmail.com>
2018-11-22 00:11:45 +0600

    update prothom-alo script

--- scripts/prothom_alo.py
+++ scripts/prothom_alo.py
@@ -1,45 +1,57 @@
 import requests
-import re
 from bs4 import BeautifulSoup
-
-url = input()
-data = requests.get(url )
-soup = BeautifulSoup(data.text,'html.parser')
-
-title = soup.find_all('title') 
-
-f = open('helloworld.html','w', encoding="utf-8")
-
-message = """<html>
-<head>
-<meta charset="UTF-8">
-</head>
-<body> """
-
-headline =  """  <h1>  """ + str (title[0].text ) + """</h1> """
+from django.http import JsonResponse
 
 
-# for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0]:
-# 	message+=str(i)
+import simplejson as json
+ 
+def news(url):
 
-image = ""
+	data = requests.get(url )
+	soup = BeautifulSoup(data.text,'html.parser')
 
-for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0].find_all('img'):
-	image+=str(i)
+	title = soup.find_all('title') 
+
+	message = """<html>
+	<head>
+	<meta charset="UTF-8">
+	</head>
+	<body> """
+
+	headline =  """  <h2>  """ + str (title[0].text ) + """</h2> """
 
 
-body=str(soup.find_all('noscript',{'id':'ari-noscript'} )[0].text  ) + "<br>"
+	# for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0]:
+	# 	message+=str(i)
 
-message+=headline
-message+=body
-message+=image
-message+=image
+	image = ""
+
+	for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0].find_all('img'):
+		tmp = i
+		tmp["height"]="200" 
+		tmp ["width"]="250"
+		image+=str(tmp)
 
 
-message+= """</body>
-</html>"""
+	body=str(soup.find_all('noscript',{'id':'ari-noscript'} )[0].text  )
 
-f.write(message)
-f.close()
+	message+="<br>"
+	message+=headline
+	message+="<br>"
+	message+=body
+	message+="<br>"
+	message+="<br>"
+	message+=image 
 
+
+	message+= """</body>
+	</html>"""
+	
+	data = {}
+
+	data ['headline'] = headline
+	data ['body'] = body
+	data ['image'] = image 
+	data['message'] = message
+	return data
 
