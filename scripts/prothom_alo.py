b661677cb0003abee8f348769337aa6c5f7401c6
Kimbbakar <chowdhuryosman04@gmail.com>
2018-11-23 21:28:03 +0600

    Update prothom_alo.py

--- scripts/prothom_alo.py
+++ scripts/prothom_alo.py
@@ -9,34 +9,42 @@ def news(url):
 
 	data = requests.get(url )
 	soup = BeautifulSoup(data.text,'html.parser')
+ 
+	message = """
+	<html> 
+		<head> 
+			<meta charset="UTF-8">
+		</head>
+	
+		<body> 
+	"""
 
-	title = soup.find_all('title') 
+	headline =  str (soup.find_all('title')[0].text )  
 
-	message = """<html>
-	<head>
-	<meta charset="UTF-8">
-	</head>
-	<body> """
-
-	headline =  """  <h2>  """ + str (title[0].text ) + """</h2> """
-
-
-	# for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0]:
-	# 	message+=str(i)
 
 	image = ""
 
-	for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0].find_all('img'):
-		tmp = i
-		tmp["height"]="200" 
-		tmp ["width"]="250"
-		image+=str(tmp)
+	if len(soup.find_all('noscript',{'id':'ari-image-1566436'} ) ) :
+		for i in soup.find_all('noscript',{'id':'ari-image-1566436'} )[0].find_all('img'):
+			tmp = i
+			tmp ["height"] ="200" 
+			tmp ["width"]  ="250"
+			image+=str(tmp)
+
+
+	if len(soup.find_all('noscript',{'id':'ari-noscript'} ) ) :
+		for i in soup.find_all('noscript',{'id':'ari-noscript'} )[0].find_all('img'):
+			tmp = i
+			tmp["height"] ="200" 
+			tmp ["width"] ="250"
+			image+=str(tmp)
+
 
 
 	body=str(soup.find_all('noscript',{'id':'ari-noscript'} )[0].text  )
 
 	message+="<br>"
-	message+=headline
+	message+="""  <h2>  """ + headline + """</h2> """
 	message+="<br>"
 	message+=body
 	message+="<br>"
@@ -44,14 +52,16 @@ def news(url):
 	message+=image 
 
 
-	message+= """</body>
-	</html>"""
+	message+= """
+		</body>
+	</html>
+	"""
 	
 	data = {}
 
 	data ['headline'] = headline
-	data ['body'] = body
-	data ['image'] = image 
-	data['message'] = message
+	data ['body']     = body
+	data ['image']    = image 
+	data ['message']  = message
 	return data
 
