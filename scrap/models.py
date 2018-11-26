e7fd13d5aa25a5c5914510b968be3c9449978af2
Kimbbakar <chowdhuryosman04@gmail.com>
2018-11-27 02:42:31 +0600

    Create News model

--- scrap/models.py
+++ scrap/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class news(models.Model):
+    portal_name  = models.CharField(max_length=20)	
+    headline 	 = models.CharField(max_length=100)
+    body 		 = models.CharField(max_length=5000)
+    img			 = models.CharField(max_length=1000)
+    ok		     = models.IntegerField();    
