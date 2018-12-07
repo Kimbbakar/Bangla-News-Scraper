054baec0a3dc3f90c678b55eb05c917c4ff3c924
Kimbbakar <chowdhuryosman04@gmail.com>
2018-12-07 15:18:36 +0600

    Update url length

--- scrap/models.py
+++ scrap/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class news(models.Model):
     portal_name  = models.CharField(max_length=20)	
-    url			 = models.CharField(primary_key=True ,max_length=200,null = False )
+    url			 = models.CharField(primary_key=True ,max_length=300,null = False )
     headline 	 = models.CharField(max_length=100)
     body 		 = models.CharField(max_length=5000)
     img			 = models.CharField(max_length=1000)
