88146926c08c2d612ec4355a87f7290e2b787226
Kimbbakar <chowdhuryosman04@gmail.com>
2018-12-13 07:30:54 +0600

    Update news body size

--- scrap/models.py
+++ scrap/models.py
@@ -6,7 +6,7 @@ class news(models.Model):
     portal_name  = models.CharField(max_length=20)	
     url			 = models.CharField(primary_key=True ,max_length=300,null = False )
     headline 	 = models.CharField(max_length=100)
-    body 		 = models.CharField(max_length=5000)
+    body 		 = models.CharField(max_length=10000)
     img			 = models.CharField(max_length=2000)
     email		 = models.CharField(max_length=50,default = "Unknown" ) 
     date		 = models.DateTimeField(default = datetime.date.today , blank=True)
